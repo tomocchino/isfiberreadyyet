05ac4ade1756f129dfe17700d89ea72036235079
tomocchino <tomocchino@gmail.com>
2016-10-18 09:03:42 -0700

    Actually pull data from GitHub rather than hard coding it

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import './css/reset.css';
 import './css/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+fetch('https://raw.githubusercontent.com/facebook/react/facts/fiber-tests.txt')
+.then(response => response.text())
+.then((data) => {
+  ReactDOM.render(
+    <App data={data} />,
+    document.getElementById('root')
+  );
+});
