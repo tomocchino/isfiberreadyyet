53869f3fe9f7d60ca4c3e4224eae67c445d44539
tomocchino <tomocchino@gmail.com>
2016-11-01 04:01:59 -0700

    adding URLs to failing/passing tests in a comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import './css/reset.css';
 import './css/index.css';
 
+// https://raw.githubusercontent.com/facebook/react/master/scripts/fiber/tests-failing.txt
+// https://raw.githubusercontent.com/facebook/react/master/scripts/fiber/tests-passing.txt
+
 fetch('https://raw.githubusercontent.com/facebook/react/facts/fiber-tests.txt')
 .then(response => response.text())
 .then((data) => {
