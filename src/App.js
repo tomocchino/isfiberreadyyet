7fc24d0ae2176e9a493ca704c5fd45a2a931d438
tomocchino <tomocchino@gmail.com>
2016-11-12 14:06:54 -0800

    use an iife around LineGraph to stop reassigning all the vars

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function IsItReady(props) {
   );
 }
 
-function LineGraph(props) {
+let LineGraph = (function() {
   // const GREEN = "#8ec56a";
   const BLACK = "#262626";
   const DGRAY = "#666";
@@ -108,42 +108,44 @@ function LineGraph(props) {
     },
   };
 
-  let start = props.data[0].date;
-  let end = props.data[props.data.length - 1].date;
-  let tickValues = [start, end];
+  return function(props) {
+    let start = props.data[0].date;
+    let end = props.data[props.data.length - 1].date;
+    let tickValues = [start, end];
 
-  return (
-    <div className="LineGraph">
-      <svg viewBox={`0 0 ${props.width - 30} 230`}>
-        <g transform={"translate(0, -40)"}>
-          <VictoryChart width={props.width} height={250}>
-            <VictoryAxis
-              scale="time"
-              standalone={false}
-              style={styles.axisDate}
-              tickValues={tickValues}
-              tickFormat={(date) => `${date.getMonth() + 1}/${date.getDate()}`}
-            />
-            <VictoryAxis
-              dependentAxis
-              domain={[0, 100]}
-              style={styles.axisPercent}
-              tickValues={[0, 25, 50, 75, 100]}
-              tickFormat={(x) => `${x}%`}
-            />
-            <VictoryLine
-              data={props.data}
-              domain={{x: [start, end], y: [0, 100]}}
-              interpolation="basis"
-              scale={{x: "time", y: "linear"}}
-              style={styles.line}
-            />
-          </VictoryChart>
-        </g>
-      </svg>
-    </div>
-  );
-}
+    return (
+      <div className="LineGraph">
+        <svg viewBox={`0 0 ${props.width - 30} 230`}>
+          <g transform={"translate(0, -40)"}>
+            <VictoryChart width={props.width} height={250}>
+              <VictoryAxis
+                scale="time"
+                standalone={false}
+                style={styles.axisDate}
+                tickValues={tickValues}
+                tickFormat={(date) => `${date.getMonth() + 1}/${date.getDate()}`}
+              />
+              <VictoryAxis
+                dependentAxis
+                domain={[0, 100]}
+                style={styles.axisPercent}
+                tickValues={[0, 25, 50, 75, 100]}
+                tickFormat={(x) => `${x}%`}
+              />
+              <VictoryLine
+                data={props.data}
+                domain={{x: [start, end], y: [0, 100]}}
+                interpolation="basis"
+                scale={{x: "time", y: "linear"}}
+                style={styles.line}
+              />
+            </VictoryChart>
+          </g>
+        </svg>
+      </div>
+    );
+  }
+})();
 
 function HeatMap(props) {
   let groups = {};
